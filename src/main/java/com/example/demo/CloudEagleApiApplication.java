081d7d8411ef3a88789d916d7db4efd9dfc26251
suraj.kadam <suraj.kadam@zeuslearning.com>
2023-02-26 01:41:12 +0530

    firstCommit

--- src/main/java/com/example/demo/CloudEagleApiApplication.java
+++ src/main/java/com/example/demo/CloudEagleApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CloudEagleApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CloudEagleApiApplication.class, args); 
+		SpringApplication.run(CloudEagleApiApplication.class, args);
 	}
 
 }
